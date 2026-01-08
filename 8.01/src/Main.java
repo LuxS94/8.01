71e93cd46f1632cefa685f35d394cb9d00a79cb4
LuxS94 <simonetti.lucio@libero.it>
2026-01-08 17:29:44 +0100

    v

--- 8.01/src/Main.java
+++ 8.01/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     static void main() {
-//        Rettangolo a = new Rettangolo(6.1, 2.1);
-//        a.stampaRettangolo();
-//        Rettangolo.getnRettangoli();
-//        Rettangolo b = new Rettangolo(2.0, 3.5);
-//        Rettangolo.stampaDueRettangoli(a, b);
+        Rettangolo a = new Rettangolo(6.1, 2.1);
+        a.stampaRettangolo();
+        Rettangolo.getnRettangoli();
+        Rettangolo b = new Rettangolo(2.0, 3.5);
+        Rettangolo.stampaDueRettangoli(a, b);
 
         Chiamate call = new Chiamate(12.1, 329887645);
         SIM sim = new SIM(329084758, 0);
